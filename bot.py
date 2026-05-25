275c28228dde40e48bc3b1f7a4b561970b14b1a6
𝐒𝐇𝐀𝐃𝐎𝐖 𝐅𝚰𝐆𝐇𝐓𝐄𝐑🔱 <shadowfyter001@gmail.com>
2026-05-25 16:09:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,23 @@
 import asyncio
-
 from telegram.ext import ApplicationBuilder
-
 from config import TOKEN
-
 from handlers.start import get_handlers
 from handlers.admin import get_admin_handlers
 from database.db import init_db
 
-
-# ================= EVENT LOOP FIX =================
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-
-# ================= INIT DATABASE =================
 loop.run_until_complete(init_db())
 
-
-# ================= BOT APP =================
 app = ApplicationBuilder().token(TOKEN).build()
 
+for h in get_handlers():
+    app.add_handler(h)
 
-# ================= HANDLERS REGISTER =================
+for h in get_admin_handlers():
+    app.add_handler(h)
 
-# user handlers
-for handler in get_handlers():
-    app.add_handler(handler)
+print("BOT RUNNING")
 
-# admin handlers
-for handler in get_admin_handlers():
-    app.add_handler(handler)
-
-
-print("🤖 BOT RUNNING...")
-
-
-# ================= START BOT =================
 app.run_polling()
