d7de4f55cf74d38d82ec06126dd48f42437c5a95
𝐒𝐇𝐀𝐃𝐎𝐖 𝐅𝚰𝐆𝐇𝐓𝐄𝐑🔱 <shadowfyter001@gmail.com>
2026-05-25 20:02:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,22 +5,19 @@ from handlers.start import get_handlers
 from handlers.admin import get_admin_handlers
 from database.db import init_db
 
-# ================= EVENT LOOP =================
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-# ================= INIT DB =================
 loop.run_until_complete(init_db())
 
-# ================= APP =================
 app = ApplicationBuilder().token(TOKEN).build()
-
-# ================= ADMIN HANDLERS (IMPORTANT FIRST) =================
-for h in get_admin_handlers():
-    app.add_handler(h)
 
 # ================= USER HANDLERS =================
 for h in get_handlers():
+    app.add_handler(h)
+
+# ================= ADMIN HANDLERS =================
+for h in get_admin_handlers():
     app.add_handler(h)
 
 print("BOT RUNNING...")
