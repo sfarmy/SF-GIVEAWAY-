4acff3cf2ad066a54c2fdfdaa976d0a0eb6d4f0a
𝐒𝐇𝐀𝐃𝐎𝐖 𝐅𝚰𝐆𝐇𝐓𝐄𝐑🔱 <shadowfyter001@gmail.com>
2026-05-28 16:01:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,31 @@
 import asyncio
+
 from telegram.ext import ApplicationBuilder
+
 from config import TOKEN
+
 from handlers.start import get_handlers
 from handlers.admin import get_admin_handlers
+from handlers.reward import get_reward_handlers
+
 from database.db import init_db
 
-# ================= EVENT LOOP =================
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
 loop.run_until_complete(init_db())
 
-# ================= APP =================
 app = ApplicationBuilder().token(TOKEN).build()
 
-# ================= ADMIN HANDLERS (FIRST SAFE ROUTE) =================
 for handler in get_admin_handlers():
     app.add_handler(handler)
 
-# ================= USER HANDLERS =================
 for handler in get_handlers():
     app.add_handler(handler)
 
-print("BOT RUNNING...")
+for handler in get_reward_handlers():
+    app.add_handler(handler)
 
+
+print("BOT RUNNING...")
 app.run_polling()
