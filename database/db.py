46b598cc3d138b2d54a73093d08763914a9e7a52
𝐒𝐇𝐀𝐃𝐎𝐖 𝐅𝚰𝐆𝐇𝐓𝐄𝐑🔱 <shadowfyter001@gmail.com>
2026-06-08 15:18:10 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -429,3 +429,25 @@ async def claim_qualified_reward(user_id):
         await db.commit()
 
         return "success"
+        
+        
+# ================= CLAIM CHECK =================
+async def can_claim_reward(user_id):
+
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        cur = await db.execute("""
+            SELECT referrals, reward_claimed
+            FROM users
+            WHERE user_id=?
+        """, (user_id,))
+
+        row = await cur.fetchone()
+
+        if not row:
+            return False
+
+        referrals, claimed = row
+
+        return referrals >= 15 and claimed == 0        
+        
